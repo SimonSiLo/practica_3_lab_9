701e4bfd3c038763b14148a769845e593e3bdc86
SimonMousen <simonuwusierra@gmail.com>
2025-08-29 14:59:26 -0500

    feat: implementar función de división con validación

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ function restar(num1, num2) {
 function multiplicar(num1, num2) {
   return num1 * num2;
 }
+function dividir(num1, num2) {
+  if (num2 === 0) {
+    return "Error: No se puede dividir por cero";
+  }
+  return num1 / num2;
+}
